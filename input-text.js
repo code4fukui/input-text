c719ecaaee7ac3e2b2c37a63af63c36ca15d5242
Taisuke Fukuno <fukuno@jig.jp>
2023-05-25 22:37:42 +0900

    add css, word-break: break-all

--- input-text.js
+++ input-text.js
@@ -6,7 +6,7 @@ export class InputText extends HTMLElement {
     this.style.border = "1px solid black";
     this.style.padding = ".3em";
     this.style.margin = ".1em";
-    this.style.wordWrap = "break-word";
+    this.style.wordBreak = "break-all";
     this.style.lineHeight = "1.2";
     this.style.display = "inline-block";
     this.value = s || this.getAttribute("value");
