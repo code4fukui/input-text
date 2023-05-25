3164876cd18f2098b4d6b0ab2336eff3abfce31a
Taisuke Fukuno <fukuno@jig.jp>
2023-05-25 22:22:11 +0900

    Initial commit

---
+++ input-text.js
@@ -0,0 +1,22 @@
+export class InputText extends HTMLElement {
+  constructor(s) {
+    super();
+    this.contentEditable = true;
+    this.style.boxSizing = "border-box";
+    this.style.border = "1px solid black";
+    this.style.padding = ".3em";
+    this.style.margin = ".1em";
+    this.style.wordWrap = "break-word";
+    this.style.lineHeight = "1.2";
+    this.style.display = "inline-block";
+    this.value = s;
+  }
+  set value(s) {
+    this.textContent = s;
+  }
+  get value() {
+    return this.textContent;
+  }
+};
+
+customElements.define("input-text", InputText);
